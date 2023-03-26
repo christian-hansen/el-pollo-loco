e22188107eac6cb693cb2300b7ec1ad237cd004e
Christian Hansen <68639582+christian-hansen@users.noreply.github.com>
2023-03-26 20:16:56 +0200

    loadimg

--- models/movable-objects.class.js
+++ models/movable-objects.class.js
@@ -3,6 +3,12 @@ class MovableObject {
     y = 400;
     img;
 
+    // loadImage('img/test.png')
+    loadImage(path) {
+        this.img = new Image(); //<img>
+        this.img.src = path;
+    }
+
     moveLeft() {
     }
 
